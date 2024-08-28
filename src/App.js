d75c06159d574c543d475fda6afff5824500a962
Enes Sağıroğlu <enes-2001sagroglu@hotmail.com>
2024-08-29 00:23:06 +0300

    address container gelistirmesi suruyor, main content bitti

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
-
-import './index.css';
-import Navbar from './components/Navbar.jsx';
+import "./index.css";
+import Navbar from "./components/Navbar.jsx";
+import MainContent from "./components/MainContent.jsx";
 
 function App() {
   return (
     <>
-    <header>
-      <Navbar />
-    </header>
-    sa
+      <header>
+        <Navbar />
+      </header>
+      <main>
+        <MainContent />
+
+      </main>
+      sa
     </>
   );
 }
---
+++ src/components/MainContent.jsx
@@ -0,0 +1,24 @@
+export default function MainContent() {
+  return (
+    <>
+      <div className="main-container">
+        <div className="main-content-container">
+          
+            <h1 className="content-h1">
+              Yemek ya da market, tüm ihtiyaçların kapında!
+            </h1>
+            <div className="address-container">
+                <div className="location-form">
+                    <div className="search-container">
+                    <input type="text" className="input-box" placeholder="Adresini Belirle veya Seç" />
+                    </div>
+                    
+                </div>
+            </div>
+
+          
+        </div>
+      </div>
+    </>
+  );
+}
